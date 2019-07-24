867fd046fea5a7a64754ededd7fda272a6a13d0a
Jhonata Rodrigues <jhonata.a.r@hotmail.com>
2019-07-23 22:18:29 -0300

    api youtube

--- src/components/videoPreview.jsx
+++ src/components/videoPreview.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default props => (
     <div className="boxVideo">
         <div className="space">
-            <div className="content">
-            
+            <div className="content" style={{ background: 'url(' + props.thumb + ') center no-repeat' }}>
+
+                <div className="title">
+                    {props.title}
+                </div>
             </div>
         </div>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,32 @@ import Nav from '../components/nav'
 import Video from '../components/videoPreview'
 
 class home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []}
+    }
+
+    componentDidMount() {
+        let playlistId = 'PL6t93nUFQQ1ZiXMfhPyhjb0PX3LgEVMcF';
+        let key = 'AIzaSyBVZQbSIU6INnKeu9pBrbRj0QzXPa74e0Y';
+        let max = '10';
+
+        fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=' + max + '&playlistId=' + playlistId + '&key=' + key, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+          }).then(response => response.json()).then(data => {
+            this.setState({videos: data.items});
+            console.log(this.state.videos);
+        });
+
+
+    }
+
     render() {
         return (
             <section className="pageHome">
@@ -15,10 +41,13 @@ class home extends Component {
                 </div>
                 <div className="rightColumn">
                     <div className="contentScroll">
-                        <Video/>
-                        <Video/>
-                        <Video/>
-                        <Video/>
+                        {this.state.videos.map((value, index) => {
+                            return <Video 
+                                        key={index} 
+                                        title={value.snippet.title} 
+                                        thumb={value.snippet.thumbnails.high.url}
+                                    />
+                        })}
                     </div>
                 </div>
             </section>
